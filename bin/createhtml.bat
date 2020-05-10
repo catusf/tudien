17f2b9aa1957724e92c6becd2a3c0e14342023fe
catusf <catusf@users.noreply.github.com>
2020-05-10 12:28:05 +0700

    Update code to integrate inflections to dictionary generation code

--- bin/createhtml.bat
+++ bin/createhtml.bat
@@ -1,1 +1,2 @@
-python tab2opf.py -utf TudienAnhVietBeta.txt
+python tab2opf.py -s vi -t vi ../dict/TudienThienChuu.tab -i ../dict/TudienThienChuu-Inflections.txt 
+python tab2opf.py -s en -t vn ../dict/TudienAnhVietBeta.tab -i ../dict/TudienAnhVietBeta-Inflections.txt
