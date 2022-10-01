ca5c2a86b9469910a8c762617ba05a0b2b244247
catus felis <catus.f@gmail.com>
2022-10-01 15:31:09 +0000

    Update code

--- bin/createhtml.bat
+++ bin/createhtml.bat
@@ -1,4 +1,4 @@
-python tab2opf.py -s vi -t vi ../dict/cactudienphathoc.tab -i ../dict/cactudienphathoc.txt 
+python ./bin/tab2opf.py -s vi -t vi ./dict/cactudienphathoc.tab -i ./dict/cactudienphathoc.txt 
 
-move /Y *.html ../dict
-move /Y *.opf ../dict
+move /Y *.html ./dict
+move /Y *.opf ./dict
--- bin/taotudienphathoc.py
+++ bin/taotudienphathoc.py
@@ -2,7 +2,7 @@ import os
 import json
 
 filepath = './dict/cactudienphathoc.json'
-fileout = './dict/cactudienphathoc.txt'
+fileout = './dict/cactudienphathoc.tab'
 
 dictdata = json.load(open(filepath, 'rb'))
 
@@ -11,8 +11,20 @@ print(dictdata.keys())
 entries = dictdata['WordRaw']
 print('Num of entries: {}'.format(len(entries)))
 
+count = 0
 with open(fileout, 'w', encoding='utf-8') as f:
     for i in entries:
-        f.write('%s\t%s\n' % (i['Word'], i['Mean']) )
+        key = i['Word'].strip()
+        meaning = i['Mean'].strip()
 
-print('Done')
+        if key.find('\n') >= 0:
+            key = key.replace('\r\n', '|')
+
+        if meaning.find('\n') >= 0:
+            meaning = meaning.replace('\r\n', '|')
+
+        if len(meaning):
+            f.write('%s\t%s\n' % (i['Word'], meaning) )
+            count = count+1
+
+print(f'{count} words processed')
