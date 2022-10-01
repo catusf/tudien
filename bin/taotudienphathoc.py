e76aab61c07d0d41b13d714785c3fd396f274e02
catus felis <catus.f@gmail.com>
2022-10-01 12:01:10 +0000

    Add code to gen dict

--- bin/taotudienphathoc.py
+++ bin/taotudienphathoc.py
@@ -1,3 +1,15 @@
 import os
+import json
 
-print("hello")
+filepath = './dict/cactudienphathoc.json'
+fileout = './dict/cactudienphathoc.txt'
+
+dictdata = json.load(open(filepath, 'rb'))
+
+print(dictdata.keys())
+
+with open(fileout, 'w', encoding='utf-8') as f:
+    for i in dictdata['WordRaw']:
+        f.write('%s\t%s\n' % (i['Word'], i['Mean']) )
+
+pass
