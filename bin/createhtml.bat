947de97d27f34a5c279b3f9bfc4c21c2200b2abf
catus felis <catus.f@gmail.com>
2022-10-01 15:47:54 +0000

    Creating html files

--- bin/createhtml.bat
+++ bin/createhtml.bat
@@ -1,4 +1,4 @@
-python ./bin/tab2opf.py -s vi -t vi ./dict/cactudienphathoc.tab -i ./dict/cactudienphathoc.txt 
+python ./bin/tab2opf.py -s vi -t vi ./dict/cactudienphathoc.tab -i ./dict/cactudienphathoc-inflections.txt 
 
 move /Y *.html ./dict
 move /Y *.opf ./dict
