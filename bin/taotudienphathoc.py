5077f0ef5bb298c2e4091af392f41c7a253baee9
catus felis <catus.f@gmail.com>
2022-10-01 14:59:13 +0000

    Add code to gen dict

--- bin/taotudienphathoc.py
+++ bin/taotudienphathoc.py
@@ -8,8 +8,11 @@ dictdata = json.load(open(filepath, 'rb'))
 
 print(dictdata.keys())
 
+entries = dictdata['WordRaw']
+print('Num of entries: {}'.format(len(entries)))
+
 with open(fileout, 'w', encoding='utf-8') as f:
-    for i in dictdata['WordRaw']:
+    for i in entries:
         f.write('%s\t%s\n' % (i['Word'], i['Mean']) )
 
-pass
+print('Done')
