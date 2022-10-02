d17b191b06c52586c0ae2820fbe2d906e2c116e6
catus felis <catus.f@gmail.com>
2022-10-02 05:14:39 +0000

    Update bat file"

--- bin/createhtml.sh
+++ bin/createhtml.sh
@@ -10,11 +10,17 @@ extension="${1##*.}"
 BASE="${1%.*}"
 
 echo $BASE
+echo "./dict/$BASE.opf.patch"
 
 cd "/workspaces/tudien/"
 
+# Generates html and opf files
 python ./bin/tab2opf.py -s vi -t vi ./dict/$1 -i ./dict/$2
 
+# Move files to final directory
 mkdir ./dict/$BASE
 mv -f *.html ./dict/$BASE/
 mv -f *.opf ./dict/$BASE/
+
+# Apply patch to fix resultant .opf file
+git apply "./dict/$BASE.opf.patch"
