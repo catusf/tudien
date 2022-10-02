c97b2742d66718ff1827e1ef644dc452c1b4aa54
catus felis <catus.f@gmail.com>
2022-10-02 04:58:37 +0000

    Update bat file

--- bin/createhtml.sh
+++ bin/createhtml.sh
@@ -2,6 +2,7 @@
 # Example: ./bin/createhtml.sh Tu-dien-Tong-hop-Phat-hoc.tab NoInflections.txt
 # Run in root directory
 
+echo "Current directory: " + $pwd
 echo $1
 echo $2
 
@@ -9,6 +10,8 @@ extension="${1##*.}"
 BASE="${1%.*}"
 
 echo $BASE
+
+cd "/workspaces/tudien/"
 
 python ./bin/tab2opf.py -s vi -t vi ./dict/$1 -i ./dict/$2
 
