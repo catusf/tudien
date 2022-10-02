d903eebf13d4e1160f9116cfc2410608c7702ec3
catus felis <catus.f@gmail.com>
2022-10-02 06:38:30 +0000

    Gen new dicts

---
+++ bin/createallhtml.sh
@@ -0,0 +1,12 @@
+./bin/createhtml.sh Ngu-vung-Danh-tu-Thien-hoc NoInflections.txt
+./bin/createhtml.sh Phat-Quang-Dai-tu-dien-(Han-ngu) NoInflections.txt
+./bin/createhtml.sh Rong-mo-tam-hon NoInflections.txt
+./bin/createhtml.sh TudienAnhVietBeta NoInflections.txt
+./bin/createhtml.sh Tu-dien-Dao-Uyen NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-hoc-Anh-Han-Viet NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-hoc-Tinh-tuyen NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc NoInflections.txt
+./bin/createhtml.sh Tu-dien-Phat-Quang NoInflections.txt
+./bin/createhtml.sh Tu-dien-Tong-hop-Phat-hoc NoInflections.txt
