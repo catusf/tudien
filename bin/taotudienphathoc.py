42cc9bf9985435dd03c01a61164c761598fcf549
catus felis <catus.f@gmail.com>
2022-10-02 01:12:14 +0000

    Update dicts

--- bin/taotudienphathoc.py
+++ bin/taotudienphathoc.py
@@ -11,6 +11,9 @@ print(dictdata.keys())
 entries = dictdata['WordRaw']
 print('Num of entries: {}'.format(len(entries)))
 
+dictionaries = {}
+
+use_dicts = {3, 7, 2, 1, 9, 6, 10, 5, 4}
 count = 0
 with open(fileout, 'w', encoding='utf-8') as f:
     for i in entries:
@@ -22,10 +25,18 @@ with open(fileout, 'w', encoding='utf-8') as f:
 
         if meaning.find('\n') >= 0:
             meaning = meaning.replace('\r\n', '|')
+        
+        dk = i['Dict']
 
-        if len(meaning):
+        if len(meaning) and dk in use_dicts:
             f.write('%s\t%s\n' % (key, meaning))
+
+            if dk not in dictionaries:
+                dictionaries[dk] = []
+            else:
+                dictionaries[dk].append(i)
 
             count = count + 1
 
+print(dictionaries.keys())
 print(f'{count} words processed')
