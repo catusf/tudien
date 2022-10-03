91eaea97dc92e3058162718e59b770aa3eb1b76d
Catus Phan <catus.phan@gmail.com>
2022-10-03 10:36:45 +0700

    Update file structure, and use of Bash instead of DOS

---
+++ bin/createallmobi.sh
@@ -0,0 +1,14 @@
+# Generate .mobi ebooks
+./bin/createmobi.sh Tu-dien-Tong-hop-Phat-hoc
+./bin/createmobi.sh Ngu-vung-Danh-tu-Thien-hoc
+./bin/createmobi.sh Phat-Quang-Dai-tu-dien-(Han-ngu)
+./bin/createmobi.sh Rong-mo-tam-hon
+./bin/createmobi.sh TudienAnhVietBeta
+./bin/createmobi.sh Tu-dien-Dao-Uyen
+./bin/createmobi.sh Tu-dien-Phat-hoc-Anh-Han-Viet
+./bin/createmobi.sh Tu-dien-Phat-hoc-Tinh-tuyen
+./bin/createmobi.sh Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai
+./bin/createmobi.sh Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong
+./bin/createmobi.sh Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc
+./bin/createmobi.sh Tu-dien-Phat-Quang
+./bin/createmobi.sh TudienThienChuu
