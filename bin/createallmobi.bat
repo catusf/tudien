293eb982f728d9bab1492640b8267c1e941f4d66
Catus Phan <catus.phan@gmail.com>
2022-10-03 08:41:01 +0700

    Create .mobi dicts

---
+++ bin/createallmobi.bat
@@ -0,0 +1,14 @@
+Rem Generate .mobi ebooks
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Tong-hop-Phat-hoc
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Ngu-vung-Danh-tu-Thien-hoc
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Phat-Quang-Dai-tu-dien-(Han-ngu)
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Rong-mo-tam-hon
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  TudienAnhVietBeta
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Dao-Uyen
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-hoc-Anh-Han-Viet
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-hoc-Tinh-tuyen
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  Tu-dien-Phat-Quang
+C:\Users\Dan\Documents\Code\tudien\bin\createmobi.bat  TudienThienChuu
