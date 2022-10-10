187d5e5690d71f4ba0d21d9e6d8877dba5a40dd4
catus felis <catus.f@gmail.com>
2022-10-10 08:07:37 +0000

    Add script to convert to epub dict

---
+++ bin/convert2epub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyglossary --source-lang=vi --target-lang=vi --name="Ngữ vựng Danh từ Thiền học" ./output/stardict/Ngu-vung-Danh-tu-Thien-hoc.ifo ./dict/Ngu-vung-Danh-tu-Thien-hoc.epub
