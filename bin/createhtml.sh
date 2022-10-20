e2da2aa10c3c19f7763a2546dba603f6b113b4d9
catus felis <catus.f@gmail.com>
2022-10-20 12:08:03 +0000

    Update folders

--- bin/createhtml.sh
+++ bin/createhtml.sh
@@ -2,21 +2,21 @@
 # Example: ./bin/createhtml.sh NoInflections.txt Ngu-vung-Danh-tu-Thien-hoc.tab
 # Run in root directory
 
-echo $1
-echo $2
+# echo $1
+# echo $2
 
 BASE="${2%.*}"
 
-echo $BASE
-echo "./dict/$BASE.opf.patch"
+# echo $BASE
+# echo "./dict/$BASE.opf.patch"
 
-cd "/workspaces/tudien/"
+# cd "/workspaces/tudien/"
 
 # Generates html and opf files
 python ./bin/tab2opf.py -s vi -t vi ./dict/$2 -i ./dict/$1
 
 # Move files to final directory
-mkdir ./dict/$BASE
+mkdir -p ./dict/$BASE
 mv -f *.html ./dict/$BASE/
 mv -f *.opf ./dict/$BASE/
 
