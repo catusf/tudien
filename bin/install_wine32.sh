975808035f34871b0d9faac37ff1a5236dd1db21
catus felis <catus.f@gmail.com>
2022-10-20 10:37:22 +0000

    Update action

--- bin/install_wine32.sh
+++ bin/install_wine32.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+# sudo dpkg --add-architecture i386
+
+# sudo apt update
+
+# wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+
+# sudo apt --yes install software-properties-common
+
+# sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+
+# sudo apt update
+
+# sudo apt --yes install --install-recommends winehq-devel
+
+# # sudo apt --yes install wine32
+
+# wine --version
+
+# sudo apt install -t parrot-backports wine32
+
+
 sudo dpkg --add-architecture i386
 
 sudo apt update
 
-wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+sudo apt-add-repository -r 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 
-sudo apt --yes install software-properties-common
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 
 sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 
-sudo apt update
+sudo apt-get update
 
-sudo apt --yes install --install-recommends winehq-devel
-
-# sudo apt --yes install wine32
-
-wine --version
+sudo apt install --install-recommends winehq-devel
 
 wine ./bin/mobigen.exe
-# sudo apt install -t parrot-backports wine32
