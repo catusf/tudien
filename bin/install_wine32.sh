a69c222c7468b2a8432da4f946fbf8963798e5b8
catus felis <catus.f@gmail.com>
2022-10-20 10:12:23 +0000

    Update Wine script

--- bin/install_wine32.sh
+++ bin/install_wine32.sh
@@ -6,15 +6,17 @@ sudo apt update
 
 wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 
-sudo apt install software-properties-common
+sudo apt --yes --force-yes install software-properties-common
 
 sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 
 sudo apt update
 
-sudo apt install --install-recommends winehq-stable
+sudo apt --yes --force-yes install --install-recommends winehq-stable
+
+sudo apt --yes --force-yes install wine32
 
 wine --version
 
-wine ./bin/mobigen.exe --version
+wine ./bin/mobigen.exe
 # sudo apt install -t parrot-backports wine32
