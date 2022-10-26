7ffb2c9e916a9559560c334bae7fc38dd564faad
catus felis <catus.f@gmail.com>
2022-10-26 05:06:07 +0000

    Update metadata for dictionaries

--- ext-dict/gen_dfo_kindle_files.sh
+++ ext-dict/gen_dfo_kindle_files.sh
@@ -1,8 +1,17 @@
 
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Bách khoa toàn thư" "Bách khoa toàn thư tiếng Việt" "vi" "vi" > "Bach khoa toan thu.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển tiếng Việt" "Từ điển tiếng Việt" "vi" "vi" > "Tieng Viet - Ho Ngoc Duc.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Hán ngữ" "Từ điển Hán ngữ" "zh" "vi" > "Tu dien Han ngu - hhh.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Việt - Nhật" "Anh - Việt" "ja" "vi" > "Viet - Nhat NXBVHTT.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Wikipedia tiếng Việt" "Từ điển Wikipedia tiếng Việt" "vi" "vi" > "Wikipedia.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
-printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển WordNet" "Từ điển WordNet" "en" "en" > "WordNet 2.0.dfo" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html"
+printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Bách khoa toàn thư" "Bách khoa toàn thư tiếng Việt" "vi" "vi" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html" > "Bach khoa toan thu.dfo"
+printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Hán ngữ" "Từ điển Hán ngữ" "zh" "vi" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html" > "Tu dien Han ngu - hhh.dfo"
+printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Việt - Nhật" "Anh - Việt" "ja" "vi" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html" > "Viet - Nhat NXBVHTT.dfo"
+printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Wikipedia tiếng Việt" "Từ điển Wikipedia tiếng Việt" "vi" "vi" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html" > "Wikipedia.dfo"
+printf "Name = Từ điển %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển WordNet" "Từ điển WordNet" "en" "en" "https://akishop.com.vn/tong-hop-tu-dien-cho-kindle-nd49491.html" > "WordNet 2.0.dfo"
+
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Prodict Anh-Việt Kỹ thuật" "Từ điển Prodict Anh-Việt Kỹ thuật" "en" "vi" "" > 'Prodict EV Tech.tab.dfo'
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Prodict Anh-Việt Kinh doanh" "Từ điển Prodict Anh-Việt Kinh doanh" "en" "vi" "" > 'Prodict EV business.tab.dfo'
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Prodict Việt-Anh Kỹ thuật" "Từ điển Prodict Việt-Anh Kỹ thuật" "vi" "en" "" > 'Prodict VE Tech.tab.dfo'
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Prodict Việt-Anh Kỹ thuật" "Từ điển Prodict Việt-Anh Kỹ thuật" "vi" "en" "" > 'Prodict VE business.tab.dfo'
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = %s" "Từ điển Prodict Kỹ thuật - Kinh doanh 4 trong 1" "Từ điển Prodict Kỹ thuật - Kinh doanh 4 trong 1" "en" "en" "" > Prodict_4in1_all.tab.dfo
+
+
+
+
  
