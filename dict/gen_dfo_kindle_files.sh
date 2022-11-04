26aa28f35db4c1d32c734d3eec085a0d41b91841
catus felis <catus.f@gmail.com>
2022-11-04 08:02:27 +0000

    Gen metadata for "old" dictionaries

---
+++ dict/gen_dfo_kindle_files.sh
@@ -0,0 +1,14 @@
+# Generate metadata for dictionaries
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Ngữ vựng danh từ thiền học" "Ngữ vựng danh từ thiền học" "vi" "vi" > Ngu-vung-Danh-tu-Thien-hoc.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Rộng mở tâm hồn" "Rộng mở tâm hồn" "vi" "vi" > Rong-mo-tam-hon.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Phật quang Đại từ điển Hán ngữ" "Phật quang Đại từ điển Hán ngữ" "zh" "zh" > Phat-Quang-Dai-tu-dien-Han-ngu.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Đạo Uyển" "Từ điển Đạo Uyển" "vi" "vi" > Tu-dien-Dao-Uyen.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật Quang" "Từ điển Phật Quang" "vi" "vi" > Tu-dien-Phat-Quang.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Anh-Hán-Việt" "Từ điển Phật học Anh-Hán-Việt" "en" "vi" > Tu-dien-Phat-hoc-Anh-Han-Viet.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Tinh tuyển" "Từ điển Phật học Tinh tuyển" "vi" "vi" > Tu-dien-Phat-hoc-Tinh-tuyen.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Đồng Loại" "Từ điển Phật học Việt Anh - Đồng Loại" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Minh Thông" "Từ điển Phật học Việt Anh - Minh Thông" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Thiện Phúc" "Từ điển Phật học Việt Anh - Thiện Phúc" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển tổng hợp Phật học" "Từ điển tổng hợp Phật học" "vi" "vi" > Tu-dien-Tong-hop-Phat-hoc.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Anh-Việt β v1.3" "Từ điển Anh-Việt β v1.3" "en" "vi" > TudienAnhVietBeta.dfo
+printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển từ Hán Việt Thiền Chửu" "Từ điển từ Hán Việt Thiền Chửu" "vi" "vi" > TudienThienChuu.dfo
