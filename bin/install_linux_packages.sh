18d92c2ca403bbfd92cf64015a5c4d8ccf7be5de
catus felis <catus.f@gmail.com>
2022-11-08 16:41:52 +0000

    Fix gem install

--- bin/install_linux_packages.sh
+++ bin/install_linux_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install dictzip &&
+sudo apt-get install dictzip systemd &&
 sudo apt install ruby-full &&
-sudo gem install bundler debug io-console -v 0.5.9
+sudo gem install bundler debug 
+sudo gem install io-console -v 0.5.9
+#sudo timedatectl set-timezone Asia/Ho_Chi_Minh
