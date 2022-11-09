80cf50d8ffe35d4c4fdcfbac957e39e94226e1d2
catus felis <phanmanhdan@gmail.com>
2022-11-09 17:16:21 +0700

    Update install_wine32.sh

--- bin/install_wine32.sh
+++ bin/install_wine32.sh
@@ -4,6 +4,7 @@ sudo dpkg --add-architecture i386  &&
 sudo mkdir -pm755 /etc/apt/keyrings &&
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key &&
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/winehq-bionic.sources &&
+sudo apt update &&
 sudo apt install --install-recommends winehq-stable &&
 
 # Verifies that it works 
