dbdc19c2da039c2860054f37dee8e517e27b1779
catus felis <phanmanhdan@gmail.com>
2022-12-26 17:49:34 +0700

    Fix issue of Ruby and Bundler

--- bin/install_linux_packages.sh
+++ bin/install_linux_packages.sh
@@ -3,7 +3,9 @@
 sudo apt-get update &&
 sudo apt-get install dictzip systemd &&
 sudo apt install ruby-full &&
-sudo gem install bundler &&
+ruby -v &&
+#sudo gem install bundler &&
+sudo gem install bundler -v 2.3.26 &&
 #sudo gem install io-console -v 0.5.9 &&
 
 sudo mv /etc/localtime /etc/localtime-backup &&
