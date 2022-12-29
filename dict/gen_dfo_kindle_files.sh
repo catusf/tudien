b79c08ab474e526ce001d33e79ec645ba5e954a3
catus felis <phanmanhdan@gmail.com>
2022-12-29 10:36:32 +0700

    Update metadata for dicts with URL and version

--- dict/gen_dfo_kindle_files.sh
+++ dict/gen_dfo_kindle_files.sh
@@ -1,14 +1,20 @@
 # Generate metadata for dictionaries
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Ngữ vựng danh từ thiền học" "Ngữ vựng danh từ thiền học" "vi" "vi" > Ngu-vung-Danh-tu-Thien-hoc.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Rộng mở tâm hồn" "Rộng mở tâm hồn" "vi" "vi" > Rong-mo-tam-hon.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Phật quang Đại từ điển Hán ngữ" "Phật quang Đại từ điển Hán ngữ" "zh" "zh" > Phat-Quang-Dai-tu-dien-Han-ngu.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Đạo Uyển" "Từ điển Đạo Uyển" "vi" "vi" > Tu-dien-Dao-Uyen.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật Quang" "Từ điển Phật Quang" "vi" "vi" > Tu-dien-Phat-Quang.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Anh-Hán-Việt" "Từ điển Phật học Anh-Hán-Việt" "en" "vi" > Tu-dien-Phat-hoc-Anh-Han-Viet.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Tinh tuyển" "Từ điển Phật học Tinh tuyển" "vi" "vi" > Tu-dien-Phat-hoc-Tinh-tuyen.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Đồng Loại" "Từ điển Phật học Việt Anh - Đồng Loại" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Minh Thông" "Từ điển Phật học Việt Anh - Minh Thông" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Phật học Việt Anh - Thiện Phúc" "Từ điển Phật học Việt Anh - Thiện Phúc" "vi" "en" > Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển tổng hợp Phật học" "Từ điển tổng hợp Phật học" "vi" "vi" > Tu-dien-Tong-hop-Phat-hoc.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển Anh-Việt β v1.3" "Từ điển Anh-Việt β v1.3" "en" "vi" > TudienAnhVietBeta.dfo
-printf "Name = %s\nDescription = %s\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = \nURL = " "Từ điển từ Hán Việt Thiền Chửu" "Từ điển từ Hán Việt Thiền Chửu" "vi" "vi" > TudienThienChuu.dfo
+print_to_file() {
+    printf "Name = $2 v$6\nDescription = $3\nSource = $4\nTarget = $5\nVersion = $6\nOwner/Editor = $7\nURL = $8\nInflections = $9" > $1
+}
+
+print_to_file "Ngu-vung-Danh-tu-Thien-hoc.dfo" "Ngữ vựng danh từ thiền học" "Ngữ vựng danh từ thiền học" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Rong-mo-tam-hon.dfo" "Rộng mở tâm hồn" "Rộng mở tâm hồn" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Phat-Quang-Dai-tu-dien-Han-ngu.dfo" "Phật quang Đại từ điển Hán ngữ" "Phật quang Đại từ điển Hán ngữ" "zh" "zh" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Dao-Uyen.dfo" "Từ điển Đạo Uyển" "Từ điển Đạo Uyển" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary"  ""
+print_to_file "Tu-dien-Phat-Quang.dfo" "Từ điển Phật Quang" "Từ điển Phật Quang" "vi" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Phat-hoc-Anh-Han-Viet.dfo" "Từ điển Phật học Anh-Hán-Việt" "Từ điển Phật học Anh-Hán-Việt" "en" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Phat-hoc-Tinh-tuyen.dfo" "Từ điển Phật học Tinh tuyển" "Từ điển Phật học Tinh tuyển" "vi" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Phat-hoc-Viet-Anh-Dong-Loai.dfo" "Từ điển Phật học Việt Anh - Đồng Loại" "Từ điển Phật học Việt Anh - Đồng Loại" "vi" "en" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Phat-hoc-Viet-Anh-Minh-Thong.dfo" "Từ điển Phật học Việt Anh - Minh Thông" "Từ điển Phật học Việt Anh - Minh Thông" "vi" "en" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Phat-hoc-Viet-Anh-Thien-Phuc.dfo" "Từ điển Phật học Việt Anh - Thiện Phúc" "Từ điển Phật học Việt Anh - Thiện Phúc" "vi" "en" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu-dien-Tong-hop-Phat-hoc.dfo" "Từ điển tổng hợp Phật học" "Từ điển tổng hợp Phật học" "vi" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "TudienThienChuu.dfo" "Từ điển từ Hán Việt Thiền Chửu" "Từ điển từ Hán Việt Thiền Chửu" "vi" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" ""
+
+print_to_file "TudienAnhVietAnh.dfo" "Từ điển Anh-Việt-Anh" "Từ điển Anh-Việt-Anh với các dạng từ tiếng Anh" "en" "vi" "1.0" "" "" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+print_to_file "TudienAnhVietBeta.dfo" "Từ điển Anh-Việt β" "Từ điển Anh-Việt β với các dạng từ tiếng Anh" "en" "vi" "1.3.1" "" "https://github.com/catusphan/dictionary" "english_inflections.txt"
--- ext-dict/gen_dfo_files.sh
+++ ext-dict/gen_dfo_files.sh
@@ -1,25 +1,45 @@
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Pháp - Việt" "Pháp - Việt" "fr" "vi" > star_phapviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Hàn" "Việt - Hàn" "vi" "ko" > star_viethan.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Anh" "Việt - Anh" "vi" "en" > star_vietanh.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Nga - Việt" "Nga - Việt" "ru" "vi" > star_ngaviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Pháp" "Việt - Pháp" "vi" "fr" > star_vietphap.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Tây Ban Nha - Việt" "Tây Ban Nha - Việt" "es" "vi" > star_tbnviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Tây Ban Nha" "Việt - Tây Ban Nha" "vi" "es" > star_viettbn.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Na Uy - Việt" "Na Uy - Việt" "no" "vi" > star_nauyviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Đức" "Việt - Đức" "vi" "de" > star_vietduc.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Trung - Việt" "Trung - Việt" "zh" "vi" > star_trungviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Bồ Đào Nha - Việt" "Bồ Đào Nha - Việt" "pt" "vi" > star_bdnviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Nhật - Việt" "Nhật - Việt" "ja" "vi" > star_nhatviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Đức - Việt" "Đức - Việt" "de" "vi" > star_ducviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Nga" "Việt - Nga" "vi" "ru" > star_vietnga.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Séc - Việt" "Séc - Việt" "cs" "vi" > star_secviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Việt - Nhật" "Việt - Nhật" "vi" "ja" > star_vietnhat.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Hàn - Việt" "Hàn - Việt" "ko" "vi" > star_hanviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Ý - Việt" "Ý - Việt" "it" "vi" > star_yviet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ dự án Open Vietnamese Dictionary Project (OVDP)\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = OVDP\nURL = https://github.com/catusphan/dictionary" "Anh - Việt" "Anh - Việt" "en" "vi" > star_anhviet.dfo
+# Generate metadata for dictionaries
+print_to_file() {
+    printf "Name = $2 v$6\nDescription = $3\nSource = $4\nTarget = $5\nVersion = $6\nOwner/Editor = $7\nURL = $8\nInflections = $9" > $1
+}
 
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ SPDict\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = SPDict\nURL = https://github.com/catusphan/dictionary" "Anh-Việt-Anh" "Anh-Việt-Anh" "en" "vi" > SPDict-Anh-Viet-Anh.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ SPDict\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = SPDict\nURL = https://github.com/catusphan/dictionary" "Anh-Việt" "Anh-Việt" "en" "vi" > SPDict-Anh-Viet.dfo
-printf "Name = Từ điển %s\nDescription = Từ điển %s từ SPDict\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = SPDict\nURL = https://github.com/catusphan/dictionary" "Việt-Anh" "Việt-Anh" "vi" "en" > SPDict-Viet-Anh.dfo
-printf "Name = Từ điển %s\nDescription = %s từ SPDict\nSource = %s\nTarget = %s \nVersion = 1.0\nOwner/Editor = SPDict\nURL = https://github.com/catusphan/dictionary" "English Irregular Verbs" "English Irregular Verbs" "en" "en" > SPDict-Irregular-Verbs.dfo
+# From OVDP Project
+print_to_file "star_phapviet.dfo" "Từ điển Pháp - Việt" "Pháp - Việt" "fr" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_viethan.dfo" "Từ điển Việt - Hàn" "Việt - Hàn" "vi" "ko" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_vietanh.dfo" "Từ điển Việt - Anh" "Việt - Anh" "vi" "en" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_ngaviet.dfo" "Từ điển Nga - Việt" "Nga - Việt" "ru" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_vietphap.dfo" "Từ điển Việt - Pháp" "Việt - Pháp" "vi" "fr" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_tbnviet.dfo" "Từ điển Tây Ban Nha - Việt" "Tây Ban Nha - Việt" "es" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_viettbn.dfo" "Từ điển Việt - Tây Ban Nha" "Việt - Tây Ban Nha" "vi" "es" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_nauyviet.dfo" "Từ điển Na Uy - Việt" "Na Uy - Việt" "no" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_vietduc.dfo" "Từ điển Việt - Đức" "Việt - Đức" "vi" "de" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_trungviet.dfo" "Từ điển Trung - Việt" "Trung - Việt" "zh" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_bdnviet.dfo" "Từ điển Bồ Đào Nha - Việt" "Bồ Đào Nha - Việt" "pt" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_nhatviet.dfo" "Từ điển Nhật - Việt" "Nhật - Việt" "ja" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_ducviet.dfo" "Từ điển Đức - Việt" "Đức - Việt" "de" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_vietnga.dfo" "Từ điển Việt - Nga" "Việt - Nga" "vi" "ru" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_secviet.dfo" "Từ điển Séc - Việt" "Séc - Việt" "cs" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_vietnhat.dfo" "Từ điển Việt - Nhật" "Việt - Nhật" "vi" "ja" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_hanviet.dfo" "Từ điển Hàn - Việt" "Hàn - Việt" "ko" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_yviet.dfo" "Từ điển Ý - Việt" "Ý - Việt" "it" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
+print_to_file "star_anhviet.dfo" "Từ điển Anh - Việt" "Anh - Việt" "en" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
 
+# From SuperDict (SPDict)
+print_to_file "SPDict-Anh-Viet-Anh.dfo" "Từ diển Anh-Việt-Anh SPDict" "Từ diển Anh-Việt-Anh SPDict với các dạng từ tiếng Anh" "en" "vi" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+print_to_file "SPDict-Anh-Viet.dfo" "Từ diển Anh-Việt SPDict" "Từ diển Anh-Việt SPDict với các dạng từ tiếng Anh" "en" "vi" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+print_to_file "SPDict-Viet-Anh.dfo" "Từ diển Việt-Anh SPDict" "Từ diển Việt-Anh SPDict" "vi" "en" "1.0" "SPDict" "https://github.com/catusphan/dictionary" ""
+print_to_file "SPDict-Irregular-Verbs.dfo" "English Irregular Verbs SPDict" "English Irregular Verbs SPDict" "en" "en" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+
+# # From Prodict
+print_to_file "Prodict_EV_Tech.dfo" "Từ điển Anh-Việt Kỹ thuật Prodict" "Từ điển Anh-Việt Kỹ thuật Prodict" "en" "vi" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+print_to_file "Prodict_EV_business.dfo" "Từ điển Anh-Việt Kinh doanh Prodict" "Từ điển Anh-Việt Kinh doanh Prodict" "en" "vi" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+print_to_file "Prodict_VE_Tech.dfo" "Từ điển Việt-Anh Kỹ thuật Prodict" "Từ điển Việt-Anh Kỹ thuật Prodict" "vi" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" ""
+print_to_file "Prodict_VE_business.dfo" "Từ điển Việt-Anh Kỹ thuật Prodict" "Từ điển Việt-Anh Kỹ thuật Prodict" "vi" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" ""
+print_to_file "Prodict_4in1_all.dfo" "Từ điển Anh-Việt Kỹ thuật & Kinh doanh Prodict" "Từ điểnt Anh-Việt Kỹ thuật & Kinh doanh Prodict" "en" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "english_inflections.txt"
+
+# Others
+print_to_file "Bach_khoa_toan_thu.dfo" "Bách khoa toàn thư" "Bách khoa toàn thư tiếng Việt" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Tu_dien_Han_ngu.dfo" "Từ điển Hán ngữ" "Từ điển Hán ngữ" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Viet-Nhat_NXBVHTT.dfo" "Từ điển Việt - Nhật" "Anh - Việt" "ja" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
+print_to_file "Wikipedia.dfo" "Từ điển Wikipedia tiếng Việt" "Từ điển Wikipedia tiếng Việt" "vi" "vi" "1.0" "Wikipedia" "https://github.com/catusphan/dictionary" ""
+print_to_file "WordNet2.0.dfo" "Từ điển WordNet" "Từ điển WordNet" "en" "en" "1.0" "Wikipedia" "https://github.com/catusphan/dictionary" ""
