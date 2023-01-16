aeaea0d4eb2459580a497e11e0c16f95f089db6a
catus felis <phanmanhdan@gmail.com>
2023-01-16 12:14:11 +0700

    Update dictionary versions

--- ext-dict/gen_dfo_files.sh
+++ ext-dict/gen_dfo_files.sh
@@ -19,7 +19,7 @@ print_to_file "star_nhatvietnhat.dfo" "Từ điển Nhật - Việt - Nhật" "T
 print_to_file "star_bdnviet.dfo" "Từ điển Bồ Đào Nha - Việt" "Từ điển Bồ Đào Nha - Việt với các dạng từ" "pt" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" "inflection-por.tab"
 print_to_file "star_secviet.dfo" "Từ điển Séc - Việt" "Từ điển Séc - Việt với các dạng từ" "cs" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" "inflection-ces.tab"
 print_to_file "star_yviet.dfo" "Từ điển Ý - Việt" "Từ điển Ý - Việt với các dạng từ" "it" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" "inflection-ita.tab"
-print_to_file "star_anhviet.dfo" "Từ điển Anh - Việt" "Từ điển Anh - Việt với các dạng từ" "en" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "star_anhviet.dfo" "Từ điển Anh - Việt" "Từ điển Anh - Việt với các dạng từ" "en" "vi" "1.1" "OVDP" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 print_to_file "star_nauyviet.dfo" "Từ điển Na Uy - Việt" "Từ điển Na Uy - Việt với các dạng từ" "no" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" "inflection-nno.tab"
 print_to_file "star_viethan.dfo" "Từ điển Việt - Hàn" "Từ điển Việt - Hàn" "vi" "ko" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
 print_to_file "star_vietanh.dfo" "Từ điển Việt - Anh" "Từ điển Việt - Anh" "vi" "en" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
@@ -33,17 +33,17 @@ print_to_file "star_vietnhat.dfo" "Từ điển Việt - Nhật" "Từ điển V
 print_to_file "star_hanviet.dfo" "Từ điển Hàn - Việt" "Từ điển Hàn - Việt" "ko" "vi" "1.0" "OVDP" "https://github.com/catusphan/dictionary" ""
 
 # From SuperDict (SPDict)
-print_to_file "SPDict-Anh-Viet-Anh.dfo" "Từ diển Anh-Việt-Anh SPDict" "Từ diển Anh-Việt-Anh SPDict với các dạng từ tiếng Anh" "en" "vi" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "SPDict-Anh-Viet-Anh.dfo" "Từ diển Anh-Việt-Anh SPDict" "Từ diển Anh-Việt-Anh SPDict với các dạng từ tiếng Anh" "en" "vi" "1.1" "SPDict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 print_to_file "SPDict-Anh-Viet.dfo" "Từ diển Anh-Việt SPDict" "Từ diển Anh-Việt SPDict với các dạng từ tiếng Anh" "en" "vi" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 print_to_file "SPDict-Viet-Anh.dfo" "Từ diển Việt-Anh SPDict" "Từ diển Việt-Anh SPDict" "vi" "en" "1.0" "SPDict" "https://github.com/catusphan/dictionary" ""
-print_to_file "SPDict-Irregular-Verbs.dfo" "English Irregular Verbs SPDict" "English Irregular Verbs SPDict" "en" "en" "1.0" "SPDict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "SPDict-Irregular-Verbs.dfo" "English Irregular Verbs SPDict" "English Irregular Verbs SPDict" "en" "en" "1.1" "SPDict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 
 # # From Prodict
-print_to_file "Prodict_EV_Tech.dfo" "Từ điển Anh-Việt Kỹ thuật Prodict" "Từ điển Anh-Việt Kỹ thuật Prodict" "en" "vi" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
-print_to_file "Prodict_EV_business.dfo" "Từ điển Anh-Việt Kinh doanh Prodict" "Từ điển Anh-Việt Kinh doanh Prodict" "en" "vi" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "Prodict_EV_Tech.dfo" "Từ điển Anh-Việt Kỹ thuật Prodict" "Từ điển Anh-Việt Kỹ thuật Prodict" "en" "vi" "1.1" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "Prodict_EV_business.dfo" "Từ điển Anh-Việt Kinh doanh Prodict" "Từ điển Anh-Việt Kinh doanh Prodict" "en" "vi" "1.1" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 print_to_file "Prodict_VE_Tech.dfo" "Từ điển Việt-Anh Kỹ thuật Prodict" "Từ điển Việt-Anh Kỹ thuật Prodict" "vi" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" ""
 print_to_file "Prodict_VE_business.dfo" "Từ điển Việt-Anh Kỹ thuật Prodict" "Từ điển Việt-Anh Kỹ thuật Prodict" "vi" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" ""
-print_to_file "Prodict_4in1_all.dfo" "Từ điển Anh-Việt Kỹ thuật & Kinh doanh Prodict" "Từ điểnt Anh-Việt Kỹ thuật & Kinh doanh Prodict" "en" "en" "1.0" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
+print_to_file "Prodict_4in1_all.dfo" "Từ điển Anh-Việt Kỹ thuật & Kinh doanh Prodict" "Từ điểnt Anh-Việt Kỹ thuật & Kinh doanh Prodict" "en" "en" "1.1" "Prodict" "https://github.com/catusphan/dictionary" "inflection-eng.tab"
 
 # Others
 print_to_file "Bach_khoa_toan_thu.dfo" "Bách khoa toàn thư" "Bách khoa toàn thư tiếng Việt" "vi" "vi" "1.0" "" "https://github.com/catusphan/dictionary" ""
