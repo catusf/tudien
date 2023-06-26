6814dbad3c8e0f1586619a98c78d41160a58b847
catus felis <phanmanhdan@gmail.com>
2023-06-26 17:20:12 +0700

    Add code to delete old sources

--- bin/install_wine32.sh
+++ bin/install_wine32.sh
@@ -3,9 +3,10 @@
 sudo dpkg --add-architecture i386  &&
 sudo mkdir -pm755 /etc/apt/keyrings &&
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key &&
+sudo rm /etc/apt/sources.list.d/*.sources &&
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/kinetic/winehq-kinetic.sources &&
 sudo apt update &&
 sudo apt install --install-recommends winehq-stable &&
 
 # Verifies that it works 
-wine ./bin/mobigen/mobigen.exe
+wine ./bin/mobigen/mobigen.exe -h
