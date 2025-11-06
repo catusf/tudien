00551a5b394665efac52c4de383c7404da3063f1
catus felis <phanmanhdan@gmail.com>
2025-11-06 02:29:34 +0000

    Fixes Dockerfile

--- bin/install_utilities.sh
+++ bin/install_utilities.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Install Git LFS module and fetch the big files
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash &&
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash &&
 #sudo apt-get install git-lfs &&
 #git lfs install && 
 #git lfs pull &&
-
-sudo apt-get install liblzo2-2 liblzo2-dev
 
 git submodule update --init --recursive &&
 git config pull.rebase true
