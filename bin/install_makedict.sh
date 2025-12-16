a1af7f4348a3351afd5b13514c8aa8586b575f27
catus felis <phanmanhdan@gmail.com>
2025-12-16 16:04:05 +0700

    Adds tool makedict

---
+++ bin/install_makedict.sh
@@ -0,0 +1,30 @@
+# 1. Install CMake, compiler (g++), and dependencies (expat is usually required for xdxf)
+sudo apt-get update && \
+sudo apt-get install -y cmake build-essential libexpat1-dev zlib1g-dev
+
+# 2. Clone the specific tag
+git clone --depth 1 --branch rev33 https://github.com/soshial/xdxf_makedict.git
+
+git clone --depth 1 https://github.com/Markismus/LanguageFilesPocketbookConverter.git
+
+# 3. Enter the directory
+cd xdxf_makedict
+
+# 4. Create a clean build directory (Best Practice)
+mkdir build
+cd build
+
+# 5. Configure with CMake (pointing to the parent directory where source is)
+cmake ..
+
+# 6. Build
+make
+
+# 7. Install (needs sudo)
+sudo make install
+
+# 8. Test
+makedict --help
+
+# ./makedict -i dictd -o xdxf ./TudienAnhVietBeta.index 
+# wine LanguageFilesPocketbookConverter/converter.exe ./dict.xdxf ./en/
